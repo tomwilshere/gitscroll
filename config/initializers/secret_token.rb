b8eabb614f1a46f789c14b079d515b978f20c104
Tom Wilshere <tom.wilshere@gmail.com>
2013-11-11 16:10:40 +0000

    Initialised RoR project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitscroll::Application.config.secret_token = 'add5ea6ea837eda6e51002b95d1ed98699605118304eeeb116a792db3b08f5569fe4f513af737400db81f55bcf1e9c1d2cc4c5be03f5d15897c1335884fb48c3'
