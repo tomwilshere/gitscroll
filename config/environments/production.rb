3cf59dcb2af3f7c5ea870c381186f2d2040665cf
Tom Wilshere <tom.wilshere@gmail.com>
2014-04-21 11:43:40 +0100

    Removed random rjs thing from heroku config #15

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,6 @@ Gitscroll::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-  config.action_view.debug_rjs  = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true
