4cf8f3dfd253e4cc5f4f24c8de05d29ad0e45a69
Tom Wilshere <tom.wilshere@gmail.com>
2014-04-21 11:51:07 +0100

    bug fix in project.rb #15

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,7 +6,7 @@ class Project < ActiveRecord::Base
   	require 'securerandom'
   	
   	# make repos directory if it doesn't exist
-  	Dir.mkdir("repos") if !exists?("repos")
+  	Dir.mkdir("repos") if !Dir.exists?("repos")
 
   	directory = "repos/" + SecureRandom.uuid
   	Dir.mkdir(directory)
