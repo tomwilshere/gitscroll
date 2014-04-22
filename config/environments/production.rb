e4e2494dc3eec88fa7c8543ec95d705a08f4b991
Tom Wilshere <tom.wilshere@gmail.com>
2014-04-22 23:35:06 +0100

    re-adding font precompilation to production. #16

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Gitscroll::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # font precompiling
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 end
