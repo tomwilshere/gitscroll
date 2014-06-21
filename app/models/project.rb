79ae89b2b99e9a1d1ec7f191b06a393239b51808
Tom Wilshere <tom.wilshere@gmail.com>
2014-06-21 01:46:49 +0100

    project.rb rubocop fixes.

--- app/models/project.rb
+++ app/models/project.rb
@@ -17,7 +17,7 @@ class Project < ActiveRecord::Base
 
   def init
     # Make repos directory if it doesn't exist
-    Dir.mkdir("repos") if !Dir.exists?("repos")
+    Dir.mkdir('repos') unless Dir.exist?('repos')
     Dir.mkdir self.repo_local_url
     repo = Rugged::Repository.clone_at(self.repo_remote_url, self.repo_local_url)
     walker = Rugged::Walker.new(repo)
@@ -32,7 +32,7 @@ class Project < ActiveRecord::Base
 
   # Verifies a local copy of the repo exists
   def exists
-    Dir.exists? self.repo_local_url
+    Dir.exist? self.repo_local_url
   end
 
   def destroy
@@ -40,29 +40,25 @@ class Project < ActiveRecord::Base
     super
   end
 
-  def calculateFilesToFix
-    res = Hash.new
-    fileHash = Hash.new(0)
+  def calculate_files_to_fix
+    res = {}
+    file_hash = Hash.new(0)
     commit_files = self.commit_files
-    commit_files_by_path = commit_files.group_by{|cf| cf.path}.values
+    commit_files_by_path = commit_files.group_by { |cf| cf.path }.values
     Metric.all.each do |metric|
       res[metric.id] = commit_files_by_path
-        .map{|cfs| {commit_file: cfs.last.id, score: cfs.last.file_metrics.where(:metric_id => metric.id)[0]? cfs.last.file_metrics.where(:metric_id => metric.id)[0].score : -1}}
-        .group_by{|cf| cf[:score]}.to_a
-        .sort_by{|cf| cf[0]}.reverse
+        .map { |cfs| { commit_file: cfs.last.id, score: cfs.last.file_metrics.where(:metric_id => metric.id)[0]? cfs.last.file_metrics.where(:metric_id => metric.id)[0].score : -1 } }
+        .group_by { |cf| cf[:score] }.to_a
+        .sort_by { |cf| cf[0] }.reverse
       res[metric.id].each_with_index do |files, index|
         files[1].each do |file|
-          fileHash[file[:commit_file]] = fileHash[file[:commit_file]] + index
+          file_hash[file[:commit_file]] = file_hash[file[:commit_file]] + index
         end
       end
     end
 
-    fileHash.to_a.map{|f| {project_id: self.id, commit_id: commit_files.find(f[0]).commit_id, commit_file_id: f[0], path: commit_files.find(f[0]).path, score: f[1]}}
-        .each{|f| FixFile.create(f)}
-    # fileHash.to_a.
-    #     sort_by{|f| f[1]}
-    #     .map{|f| {project_id: self.id, commit_id: self.commit_files.find(f[0]).commit_id, commit_file_id: f[0], path: self.commit_files.find(f[0]).path}}
-    #     .each{|f| FixFile.create(f)}
+    file_hash.to_a.map { |f| { project_id: self.id, commit_id: commit_files.find(f[0]).commit_id, commit_file_id: f[0], path: commit_files.find(f[0]).path, score: f[1] } }
+        .each { |f| FixFile.create(f) }
   end
 
 end
